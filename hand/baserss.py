c243c067cdc0af2d0c27efc9df6b4d4afa07a5a2
Luiz Irber <luiz.irber@gmail.com>
2013-08-21 20:29:28 -0300

    PyLint fixes

--- hand/baserss.py
+++ hand/baserss.py
@@ -1,21 +1,24 @@
 #!/usr/bin/env python
-from os.path import expanduser
-from datetime import date, timedelta, datetime
+
+from datetime import date, datetime
 from StringIO import StringIO
 import rfc822
 from ConfigParser import ConfigParser
 from string import Template
 import codecs
-import re, htmlentitydefs
+import re
+import htmlentitydefs
 
 from shove import Shove
 
-today = date(2000,1,1).today()
+
+TODAY = date(2000, 1, 1).today()
+
 
 class BaseFeedGenerator(object):
 
     def __init__(self, config_file=None):
-        #configs = config
+        # configs = config
         self.conf = {}
         if config_file:
             config = ConfigParser()
@@ -53,7 +56,7 @@ class BaseFeedGenerator(object):
                     text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
                 except KeyError:
                     pass
-            return text # leave as is
+            return text  # leave as is
         return re.sub("&#?\w+;", fixup, text)
 
     def generate_description(self, link):
@@ -61,37 +64,39 @@ class BaseFeedGenerator(object):
 
     def build_feed(self, data):
         feed = StringIO(u'')
-        pubDate = self.build_date(datetime(1,1,1).now())
+        pubDate = self.build_date(datetime(1, 1, 1).now())
         try:
-            template_data = codecs.open(self.conf['template_file'], 'r', 'utf-8')
+            template_data = codecs.open(self.conf[
+                                        'template_file'], 'r', 'utf-8')
             template = Template(template_data.read())
             template_data.close()
         except:
-            print 'error' #TODO: tratar erro!
-        base = template.substitute(self.conf, pubDate=pubDate, lastBuildDate=pubDate)
+            print 'error'  # TODO: tratar erro!
+        base = template.substitute(
+            self.conf, pubDate=pubDate, lastBuildDate=pubDate)
         feed.write(base)
 
         feed.write('    <skipHours>\n')
         build_hours = [int(i) for i in self.conf['build_hours'].split(',')]
-        for skip in xrange(0,24):
+        for skip in xrange(0, 24):
             if skip not in build_hours:
-                feed.write('      <hour>%d</hour>\n'% skip)
+                feed.write('      <hour>%d</hour>\n' % skip)
         feed.write('    </skipHours>\n')
 
         for entry in data:
-            if date(*rfc822.parsedate(entry['pubDate'])[:3]) <= today:
+            if date(*rfc822.parsedate(entry['pubDate'])[:3]) <= TODAY:
                 feed.write(
-                   ("""    <item>\n"""
-                    """      <title>%s</title>\n"""
-                    """      <link>%s</link>\n"""
-                    """      <description>%s</description>\n"""
-                    """      <pubDate>%s</pubDate>\n"""
-                    """      <guid>%s</guid>\n"""
-                    """    </item>\n""") % (entry['title'],
-                                            entry['page_link'],
-                                            entry['description'],
-                                            entry['pubDate'],
-                                            entry['guid']))
+                    ("""    <item>\n"""
+                     """      <title>%s</title>\n"""
+                     """      <link>%s</link>\n"""
+                     """      <description>%s</description>\n"""
+                     """      <pubDate>%s</pubDate>\n"""
+                     """      <guid>%s</guid>\n"""
+                     """    </item>\n""") % (entry['title'],
+                                             entry['page_link'],
+                                             entry['description'],
+                                             entry['pubDate'],
+                                             entry['guid']))
 
         feed.write("""  </channel>\n""")
         feed.write('</rss>\n')
@@ -120,7 +125,7 @@ class BaseFeedGenerator(object):
     def process(self):
         data = list(self.generate_data())
         if self.save_data(data):
-        #if 1:
+        # if 1:
             rss_feed = self.build_feed(data)
             rss_file = codecs.open(self.conf['output_file'], 'w+', 'utf-8')
             rss_file.write(rss_feed)
